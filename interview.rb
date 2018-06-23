efc3fcd4be10ebddf8606244018081d1b39bfc61
sun <yohansun@qq.com>
2018-06-23 23:09:52 +0800

    Update interview.rb

--- interview.rb
+++ interview.rb
@@ -277,3 +277,36 @@ class Node
 end
 #https://www.jianshu.com/p/d8e681b46e50
 #bfs＝队列，入队列，出队列；dfs=栈，压栈，出栈
+	    
+#mysql索引的最左匹配原则？
+#mysql存储引擎有哪些？优缺点有哪些？
+#..MyISAM引擎是一种非事务性的引擎，提供高速存储和检索，以及全文搜索能力，适合数据仓库等查询频繁的应用。MyISAM中，一个table实际保存为三个文件，.frm存储表定义，.MYD存储数据,.MYI存储索引。  NULL值被允许在索引的列中。
+#..InnoDB:一个事务安全的存储引擎
+#区别：
+1.InnoDB不支持FULLTEXT类型的索引。
+2.InnoDB 中不保存表的具体行数，也就是说，执行select count(*) from table时，InnoDB要扫描一遍整个表来计算有多少行，但是MyISAM只要简单的读出保存好的行数即可。注意的是，当count(*)语句包含 where条件时，两种表的操作是一样的。
+3.对于AUTO_INCREMENT类型的字段，InnoDB中必须包含只有该字段的索引，但是在MyISAM表中，可以和其他字段一起建立联合索引。
+4.DELETE FROM table时，InnoDB不会重新建立表，而是一行一行的删除。
+另外，InnoDB表的行锁也不是绝对的，如果在执行一个SQL语句时MySQL不能确定要扫描的范围，InnoDB表同样会锁全表，例如update table set num=1 where name like “%aaa%”
+可以用 show create table tablename 命令看表的类型。
+2.1 对不支持事务的表做start/commit操作没有任何效果，在执行commit前已经提交
+可以执行以下命令来切换非事务表到事务(数据不会丢失)，innodb表比myisam表更安全：
+alter table tablename type=innodb;
+MyISAM存储引擎的读锁和写锁是互斥的，读写操作是串行的。那么，一个进程请求某个MyISAM表的读锁，同时另一个进程也请求同一表的写锁，MySQL如何处理呢？答案是写进程先获得锁。不仅如此，即使读请求先到锁等待队列，写请求后到，写锁也会插到读锁请求之前！这是因为MySQL认为写请求一般比读请求要重要。这也正是MyISAM表不太适合于有大量更新操作和查询操作应用的原因，因为，大量的更新操作会造成查询操作很难获得读锁，从而可能永远阻塞。这种情况有时可能会变得非常糟糕！myisam是有读锁和写锁(2个锁都是表级别锁)。
+MySQL表级锁有两种模式：表共享读锁（Table Read Lock）和表独占写锁（Table Write Lock）。什么意思呢，就是说对MyISAM表进行读操作时，它不会阻塞其他用户对同一表的读请求，但会阻塞 对同一表的写操作；而对MyISAM表的写操作，则会阻塞其他用户对同一表的读和写操作。
+
+#mysql有哪几类锁？每类锁的优点和缺点
+页级:引擎 BDB。
+表级:引擎 MyISAM ， 理解为锁住整个表，可以同时读，写不行
+行级:引擎 INNODB ， 单独的一行记录加锁
+MySQL表级锁有两种模式：表共享读锁（Table Read Lock）和表独占写锁（Table Write Lock）。什么意思呢，就是说对MyISAM表进行读操作时，它不会阻塞其他用户对同一表的读请求，但会阻塞 对同一表的写操作；而对MyISAM表的写操作，则会阻塞其他用户对同一表的读和写操作。
+原子性（Atomicity）：事务是一个原子操作单元，其对数据的修改，要么全部执行，要么全都不执行；
+一致性（Consistent）：在事务开始和完成时，数据都必须保持一致状态；
+隔离性（Isolation）：数据库系统提供一定的隔离机制，保证事务在不受外部并发操作影响的“独立”环境执行；
+持久性（Durable）：事务完成之后，它对于数据的修改是永久性的，即使出现系统故障也能够保持。
+1）共享锁：允许一个事务去读一行，阻止其他事务获得相同数据集的排他锁。
+    ( Select * from table_name where ......lock in share mode)
+
+2）排他锁：允许获得排他锁的事务更新数据，阻止其他事务取得相同数据集的共享读锁和  排他写锁。(select * from table_name where.....for update)
+    为了允许行锁和表锁共存，实现多粒度锁机制；同时还有两种内部使用的意向锁（都是表锁），分别为意向共享锁和意向排他锁。
+    InnoDB行锁是通过给索引项加锁来实现的，即只有通过索引条件检索数据，InnoDB才使用行级锁，否则将使用表锁！
